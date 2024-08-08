02d01323214961761d9352280533dc763700dbcc
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-08-08 23:54:37 +0300

    feat: add soluton

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let res = [];
+    if (!matrix) {
+        return res;
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            const columns = i % 2 === 0 ? j : (matrix[i].length -j -1)
+            res.push(matrix[i][columns]);
+        }
+    }
+    return res;
 }
